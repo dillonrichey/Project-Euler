83b8415f974474dc4e42bb89077dcdd8b829dd5c
Dillon Richey <41878875+dillonrichey@users.noreply.github.com>
2022-03-03 09:29:06 -0700

    Add files via upload
    Updated addEven() method to be more time and space efficient by performing operations while iterating through sequence values, instead of first storing values in a list and reiterating.

--- Euler002/src/EvenFibonacciNumbers.java
+++ Euler002/src/EvenFibonacciNumbers.java
@@ -4,56 +4,42 @@ import java.util.Arrays;
 /**
  * @author dillonrichey
  * 
- * This program is a solution to Problem 2 from Project Euler.
+ *         This program is a solution to Problem 2 from Project Euler.
  * 
- * http://projecteuler.net/problem=002
+ *         http://projecteuler.net/problem=002
  * 
- * This program calculates the sum of the even-valued numbers in
- * the Fibonacci sequence between 0 and four million. The sequence
- * of numbers is printed to the console followed by the sum of the
- * even numbers within the sequence.
+ *         This program calculates the sum of the even-valued numbers in the
+ *         Fibonacci sequence between 0 and four million. The sequence of
+ *         numbers is printed to the console followed by the sum of the even
+ *         numbers within the sequence.
  *
  */
 public class EvenFibonacciNumbers {
 
 	public static void main(String[] args) {
-		
-		//new arraylist to store numbers from Fibonacci sequence
-		ArrayList<Integer> fibNums = new ArrayList<Integer>(2);
-		
-		
-		/*
-		 * initialize the first two elements in the array to
-		 * contain the numbers 1 and 2, respectively.
-		 */
-		fibNums.add(1);
-		fibNums.add(2);
-		
-		long fibSum = 0;
-		/*
-		 * set the initial value of the sum of even numbers in the 
-		 * Fibonacci sequence to 2 in order to avoid an "off by two"
-		 * error
-		 */
 
-		long evenFibSum = 2;
-		
-		while (fibSum < 4000000) {
-			
-			for (int i = 2; fibSum < 4000000; i++) {
-				
-				fibSum = fibNums.get(i-1) + fibNums.get(i-2);
-				fibNums.add((int) fibSum);
-				
-				if (fibSum % 2 == 0) {
-					evenFibSum += fibSum;
-				}
-			}
-			
+		System.out.println("The sum of all even fibonacci numbers less than four million is: "+ addEven(4000000));
+	}
+
+	public static int addEven(int max) {
+
+		int sum = 0;
+											/* Start Cases */
+		int first = 1;						// F(0) = 1
+		int second = 1;						// F(1) = 1
+		int third = first + second;			// F(2) = F(n-1) + F(n-2)
+											//		=   F(1) + F(0)
+											//		=      1 + 1
+											//		= 		 2
+
+		while (third < max) {
+			sum += third;					//    sum =    sum + F(n)
+			first = second + third;			// F(n-2) = F(n-1) + F(n)
+			second = third + first;			// F(n-1) =   F(n) + F(n-2)
+			third = first + second;			//   F(n) = F(n-2) + F(n-1)
 		}
-		System.out.println(Arrays.toString(fibNums.toArray()));
-		System.out.println(evenFibSum);
 
-}
-	
+		return sum;
+	}
+
 }
